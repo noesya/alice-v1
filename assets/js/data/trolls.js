df74bfb9447e6506ae3e8fe96bece623429638eb
Arnaud Levy <contact@arnaudlevy.com>
2023-06-21 16:20:46 +0200

    troll

---
+++ assets/js/data/trolls.js
@@ -0,0 +1,9 @@
+const TROLLS = {
+    images: [
+        '/assets/images/charactershot/troll.png'
+    ],
+    slangs: [
+        'Restez pauvres bandes de fils de putes',
+        'Vas-y régule moi ça ma gourmande'
+    ]
+}
