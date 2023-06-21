e16bfc57958539f344f817776f5657e3933c9d90
Arnaud Levy <contact@arnaudlevy.com>
2023-06-21 15:20:31 +0200

    wip

--- assets/js/Scene.js
+++ assets/js/Scene.js
@@ -1,15 +1,17 @@
 import Hero from './objects/Hero';
+import Map from './objects/Map';
 
 export default class Scene {
   constructor(game) {
     this.game = game;
-    console.log('i am the scene');
     this.setup();
   }
   setup() {
+    this.map = new Map();
     this.hero = new Hero();
   }
   update() {
     this.hero.update();
+    this.map.update();
   }
 }
