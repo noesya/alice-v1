854a80429cab16384537069c6af163f6faa913d8
Arnaud Levy <contact@arnaudlevy.com>
2023-06-21 17:39:53 +0200

    popin

--- assets/js/objects/Popin.js
+++ assets/js/objects/Popin.js
@@ -1,5 +1,16 @@
 import { game } from "js/Game";
 
 export default class Popin {
+    constructor(id) {
+        this.element = document.getElementById(id);
+        this.close = this.element.querySelector('.close')[0]; 
+    }
 
+    show() {
+        this.classList.add("active");
+    }
+    
+    hide() {
+        this.classList.remove("active");
+    }
 }
