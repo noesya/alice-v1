84b32f326f12954d592db6a7abba80384a1d73db
Arnaud Levy <contact@arnaudlevy.com>
2023-06-21 12:02:24 +0200

    js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
-console.log('ok');
+var alice = document.getElementsByClassName('alice')[0];
+var step = 10;
+
+
+function keyPressed(event) {
+    event = event || window.event;
+    if (event.keyCode == '38') { // up arrow
+        // console.log('up')
+        console.log(alice);
+        alice.style.top = alice.style.top - step;
+    } else if (event.keyCode == '40') { // down arrow
+        alice.style.top = alice.style.top + step;
+    } else if (event.keyCode == '37') { // left arrow
+       alice.style.top = alice.style.left - step;
+    } else if (event.keyCode == '39') { // right arrow
+        alice.style.top = alice.style.left + step;
+    }
+}
+document.onkeydown = keyPressed;
