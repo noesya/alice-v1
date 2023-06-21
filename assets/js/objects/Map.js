3fa8be05f3a2992de799cedee6cbae9f2afe6ffb
Arnaud Levy <contact@arnaudlevy.com>
2023-06-21 15:27:45 +0200

    width

--- assets/js/objects/Map.js
+++ assets/js/objects/Map.js
@@ -1,10 +1,11 @@
+import { game } from "js/Game";
 import Thing from "./Thing";
 
 export default class Map extends Thing {
   constructor() {
     super({
-      width: 1024,
-      height: 768,
+      width: game.canvas.width,
+      height: game.canvas.height,
       src: '/assets/images/map/map.jpg'
     })
   }
