245502c447dd4c24581d987a80721d71ed74ff30
Arnaud Levy <contact@arnaudlevy.com>
2023-06-21 17:53:10 +0200

    js

--- assets/js/objects/Troll.js
+++ assets/js/objects/Troll.js
@@ -62,8 +62,6 @@ export default class Troll extends Character {
     super.onCollide();
     const slang = TROLLS.slangs[Math.round(Math.random() * (TROLLS.slangs.length-1))];
     popinTroll.show();
-    // const popin = document.getElementById('pop-in-troll');
-    // popin.classList.add("active");
     popinTroll.element.querySelector('p').innerHTML = slang;
   }
   update() {
