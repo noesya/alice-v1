e7b25838b1197ebc1333b950687aaf3ee7995c0d
alexisben <alex@noesya.coop>
2023-06-21 15:22:56 +0200

    add controls

--- assets/js/objects/Hero.js
+++ assets/js/objects/Hero.js
@@ -7,15 +7,35 @@ export default class Hero extends Character {
       height: 42,
       src: '/assets/images/characters/alice/alice-character.png',
       animations: {
-        walk: {
-          steps: 5,
+        idle: {
+          steps: 4,
           y: 0
+        },
+        walk: {
+          steps: 6,
+          y: 100
         }
       }
-    })
+    });
 
     this.name = "hero"
 
-    this.setAnimation("walk")
+    this.setAnimation("walk");
+    this.setAnimation("idle");
+  
+    this.listen()
+  }
+  listen() {
+    window.addEventListener('keydown', e => {
+      if (e.code === 'ArrowRight') {
+        this.move(1, 0);
+      } else if (e.code === 'ArrowDown') {
+        this.move(0, 1);
+      } else if (e.code === 'ArrowLeft') {
+        this.move(-1, 0);
+      } else if (e.code === 'ArrowUp') {
+        this.move(0, -1);
+      }
+    })
   }
 }
