a8bd437cbcc97f06654fa065e5e14598591489a9
alexisben <alex@noesya.coop>
2023-06-22 09:13:14 +0200

    add buttons control

--- assets/js/Controls.js
+++ assets/js/Controls.js
@@ -9,6 +9,7 @@ export default class Controls {
     };
 
     this.bindKeyboard();
+    this.bindButtons();
   }
   bindKeyboard() {
     window.addEventListener('keydown', e => {
@@ -46,6 +47,19 @@ export default class Controls {
       this.actions.left = false;
       this.actions.down = false;
       this.actions.up = false;
-   }) 
+    });
+  }
+  bindButtons() {
+    const buttons = document.querySelectorAll('.game-controls button');
+    buttons.forEach(button => {
+      button.addEventListener('pointerdown', (e) => {
+        const action = button.getAttribute('data-action');
+        this.actions[action] = true;
+      })
+      button.addEventListener('pointerup', (e) => {
+        const action = button.getAttribute('data-action');
+        this.actions[action] = false;
+      })
+    })
   }
 }
