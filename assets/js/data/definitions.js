8b414aba3e7fc5769e827caa065ea73c0e4c66a2
cle0L <95277869+cle0L@users.noreply.github.com>
2023-07-05 16:02:03 +0200

    positions

--- assets/js/data/definitions.js
+++ assets/js/data/definitions.js
@@ -4,10 +4,10 @@ export const DEFINITIONS = [
     { 
         title: 'Bitcoin', 
         src: '/assets/images/coin/coin.png',
-        x: 50,
-        y: 700,
-        srcWidth: 401,
-        srcHeight: 389,
+        x: 300,
+        y: 800,
+        srcWidth: 160,
+        srcHeight: 160,
         text: 'Première monnaie appuyée sur une blockchain, inventée par Satoshi Nakamoto. Gouffre énergétique. Objet de culte.' 
     },
     {
@@ -15,109 +15,179 @@ export const DEFINITIONS = [
         src: '/assets/images/coin/coin.png',
         x: 950,
         y: 500,
-        srcWidth: 401,
-        srcHeight: 389,
+        srcWidth: 160,
+        srcHeight: 160,
         text: 'Technologie permettant d’enregistrer des lignes dans un registre, de façon plus ou moins sécurisée et plus ou moins énergivore.'
     },
     {
         title: 'Carte graphique',
         src: '/assets/images/coin/coin.png',
-        x: 1350,
-        y: 200,
-        srcWidth: 366,
-        srcHeight: 416,
+        x: 1440,
+        y: 660,
+        srcWidth: 160,
+        srcHeight: 160,
         text: 'Avant l’avènement des cryptos, une carte graphique était un composant permettant d’afficher des images. Plus maintenant ! C’est devenu un composant pour miner (cf Minage), générant une énorme augmentation de la demande : depuis 2021, en 18 mois, les mineurs de crypto-monnaies ont dépensé <a href="https://www.01net.com/actualites/15-milliards-dollars-cartes-graphiques-depenses-folles-mineurs-crypto-2021.html" target="_blank">plus de 15 milliards de dollars pour en acheter</a>.'
     },
     {
         title: 'Crypto bro',
         src: '/assets/images/coin/coin.png',
-        x: 550,
-        y: 700,
-        srcWidth: 371,
-        srcHeight: 429,
+        x: 750,
+        y: 800,
+        srcWidth: 160,
+        srcHeight: 160,
         text: 'Jeune homme plein d’enthousiasme pour les cryptos, qui partage son engouement de façon bruyante, parfois en insultant des crypto-athées.'
     },
     {
         title: 'Crypto monnaie',
         src: '/assets/images/coin/coin.png',
-        x: 2200,
+        x: 2000,
         y: 500,
-        srcWidth: 391,
-        srcHeight: 459,
+        srcWidth: 160,
+        srcHeight: 160,
         text: 'Monnaie numérique utilisant la blockchain comme socle technique, sans banque centrale et sans état. Synonymes : crypto actif, crypto devise.'
     },
     {
         title: 'Crypto troll',
         src: '/assets/images/coin/coin.png',
-        x: 2100,
+        x: 250,
         y: 250,
-        srcWidth: 265,
-        srcHeight: 387,
+        srcWidth: 160,
+        srcHeight: 160,
         text: 'Crypto bro particulièrement porté sur l’insulte et la mauvaise foi.'
     },
     {
         title: 'DAO (Decentralized Autonomous Organization)',
         src: '/assets/images/coin/coin.png',
-        x: 400,
-        y: 350,
-        srcWidth: 390,
-        srcHeight: 423,
+        x: 1950,
+        y: 300,
+        srcWidth: 160,
+        srcHeight: 160,
         text: 'Organisation magique permettant de résoudre tous les problèmes du monde. Garantit aussi le retour de l’être aimé et la puissance sexuelle.'
     },
     {
         title: 'Ethereum',
+        src: '/assets/images/coin/coin.png',
+        x: 350,
+        y: 600,
+        srcWidth: 160,
+        srcHeight: 160,
         text: 'Crypto monnaie qui promet qu’elle va bientôt passer en Proof of Stake et devenir plus écologique (moins désastreuse). Ça fait 2 ans que c’est pour bientôt. Restez en ligne. C\'est bon, c\'est fait ! Ça s\'appelait The Merge, et c\'était le 15 septembre 2022.'
     },
     {
         title: 'Fiat monnaie',
+        src: '/assets/images/coin/coin.png',
+        x: 800,
+        y: 350,
+        srcWidth: 160,
+        srcHeight: 160,
         text: 'Les pièces et les billets, aussi appelés monnaie fiduciaire. Il paraît que si on l’utilise, on devient vieux.'
     },
     {
         title: 'Minage',
+        src: '/assets/images/coin/coin.png',
+        x: 900,
+        y: 700,
+        srcWidth: 160,
+        srcHeight: 160,
         text: 'Processus qui consiste à ne rien faire de ses dix doigts, en faisant travailler les processeurs graphiques de beaucoup trop d’ordinateurs pour produire des calculs qui vont à la poubelle la plupart du temps, si c’est du POW (voir POW). Si c’est du POS (voir POS), on ne fait toujours rien de ses dix doigts, mais les ordinateurs se mettent d’accord avant de travailler. Pour ce travail, on est généralement rémunéré en crypto monnaie, ce qui est intéressant tant que la valeur de la monnaie en question dépasse le coût de l’énergie dépensée pour les calculs. Parfois, si l’électricité est trop chère, ou si les cryptos ne valent pas assez, on laisse ces pauvres ordinateurs se reposer.'
     },
     {
         title: 'Minting',
+        src: '/assets/images/coin/coin.png',
+        x: 1650,
+        y: 600,
+        srcWidth: 160,
+        srcHeight: 160,
         text: 'Processus de création d’un NFT à partir d’un objet numérique, par exemple un fichier ou une URL. On prend un bidule qu’on peut copier à l’infini, et on dépense de l’énergie pour faire croire que non.'
     },
     {
         title: 'NFT (Non Fongible Token)',
+        src: '/assets/images/coin/coin.png',
+        x: 2200,
+        y: 100,
+        srcWidth: 160,
+        srcHeight: 160,
         text: 'Un identifiant unique, enregistré dans une blockchain, qui lie un objet numérique avec le compte d’une personne. Si les NFTs sont souvent présentés comme des titres de propriété, ils n’en sont absolument pas, comme l’illustre la vente du territoire de la Russie en NFT. C’est un dispositif permettant de vendre un droit illusoire sur une création que l’on ne possède pas. Plus absurde, dans la plupart des cas, les NFTs sont utilisés pour prétendre détenir une instance unique d’un fichier dupliqué à l’infini, comme une image que l’on peut s’envoyer par mail.'
     },
     {
         title: 'Oracle',
+        src: '/assets/images/coin/coin.png',
+        x: 1500,
+        y: 380,
+        srcWidth: 160,
+        srcHeight: 160,
         text: 'C’est un grand nom pour un concept clé : le système qui raccorde une blockchain avec le monde réel. Par exemple, si je veux enregistrer des informations sur le blé utilisé pour faire un morceau de pain, il faut qu’un humain ou une machine le saisisse via une interface. Et si on lui dit n’importe quoi, et bien la blockchain enregistre ce n’importe quoi. Bref, ça sécurise la partie qui ne pose aucun problème (le registre), et ça ne sécurise pas la partie qui pose tous les problèmes (la source d’information). Reprenez un cookie, Neo.'
     },
     {
         title: 'POA (Proof of Authority)',
+        src: '/assets/images/coin/coin.png',
+        x: 1300,
+        y: 500,
+        srcWidth: 160,
+        srcHeight: 160,
         text: 'Un système dans lequel on centralise la décentralisation. Oui, c’est un contresens.'
     },
     {
         title: 'POH (Proof of History)',
+        src: '/assets/images/coin/coin.png',
+        x: 1080,
+        y: 250,
+        srcWidth: 160,
+        srcHeight: 160,
         text: 'Un genre de POS qui s’appuie sur les dates de transaction, avec un truc incroyablement nouveau, le timestamp. Le timestamp est le nombre de secondes écoulées entre une certaine date et le 1er janvier 1970 à 00:00:00, appelé aussi epoch. On se croirait dans Matrix, mais c’est juste une date, rien de spécial. Ah si, c’est la sortie de l’album “Third”, de Soft Machine.'
     },
     {
         title: 'POS (Proof of Stake)',
+        src: '/assets/images/coin/coin.png',
+        x: 2000,
+        y: 100,
+        srcWidth: 160,
+        srcHeight: 160,
         text: 'Le mécanisme utilisé par Ethereum depuis 2022. Concrètement, dans le POW, tout le monde court. Dans le POS, on choisit d’abord les coureurs, ce qui limite la course pour rien. En revanche, ça crée un nouveau problème : sur quels critères choisit-on les coureurs ? Et bien on choisit les plus riches ! Révolutionnaire, comme idée !!!'
     },
     {
         title: 'POW (Proof of Work)',
+        src: '/assets/images/coin/coin.png',
+        x: 1800,
+        y: 450,
+        srcWidth: 160,
+        srcHeight: 160,
         text: 'Le système utilisé par Bitcoin pour enregistrer les transactions. Ca ressemble beaucoup à la course des spermatozoïdes, sauf que ce serait des spermatozoïdes avec une carte graphique énorme qui consomme beaucoup d’électricité. Et à la fin, il y en a un qui devient le papa d’un joli bloc tout rose, et tous les autres vont dans les toilettes.'
     },
     {
         title: 'Satoshi Nakamoto',
+        src: '/assets/images/coin/coin.png',
+        x: 650,
+        y: 500,
+        srcWidth: 160,
+        srcHeight: 160,
         text: 'Le créateur légendaire du bitcoin. En réalité, c’est un pseudonyme, on ne sait pas qui a fait le coup, et cette question est devenue un vrai Cluedo en ligne.'
     },
     {
         title: 'Smart contract',
+        src: '/assets/images/coin/coin.png',
+        x: 1070,
+        y: 850,
+        srcWidth: 160,
+        srcHeight: 160,
         text: 'Un contrat automatisé, qui s’exécutera quoi qu’il arrive. Enfin, sauf si le site a disparu entre temps, ou qu’il y a un problème de version, ou de sécurité, ou… Penser qu’avec des ordinateurs, dans plusieurs années, tout va bien se passer, c’est si bête que c’est mignon.'
     },
     {
         title: 'Stablecoin',
+        src: '/assets/images/coin/coin.png',
+        x: 100,
+        y: 550,
+        srcWidth: 160,
+        srcHeight: 160,
         text: 'Une crypto-monnaie stable. Garantie totale ! Aucun crash, rien, jamais. Bon, ça va, c’était une blague.'
     },
     {
         title: 'Token',
+        src: '/assets/images/coin/coin.png',
+        x: 150,
+        y: 900,
+        srcWidth: 160,
+        srcHeight: 160,
         text: 'Un jeton qui se donne des grands airs. C’est juste une chaîne de caractères un peu longue.'
     }
 ];
