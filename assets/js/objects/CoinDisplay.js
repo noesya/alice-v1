524b51756dc6291a6584334b91199269a096427e
cle0L <95277869+cle0L@users.noreply.github.com>
2023-07-07 11:02:49 +0200

    coindisplay

--- assets/js/objects/CoinDisplay.js
+++ assets/js/objects/CoinDisplay.js
@@ -1,22 +1,44 @@
+ 
 import { game } from "js/Game";
 
 export default class CoinDisplay {
-    constructor() {
-      this.collectedCoins = [];
-      this.x = 10; 
-      this.y = 10; 
-    }
-  
-    addCoin(coin) {
-      this.collectedCoins.push(coin);
-    }
-  
-    draw() {
-        const ctx = game.ctx;
-
-        ctx.fillStyle = "white";
-        ctx.font = "16px Arial";
-        ctx.fillText(`Coins: ${this.collectedCoins.length}`, this.x, this.y);
-    }
+  constructor() {
+    this.coins = [];
+    this.coinImage = new Image();
+    this.coinImage.src = "/assets/images/coin/flat-coin.png";
+    this.coinImageWidth = 22;
+    this.coinImageHeight = 22;
+    this.padding = 4;
+    this.position = {
+      x: 10,
+      y: 10
+    };
+    this.coinsPerRow = 11; 
+    this.rowHeight = this.coinImageHeight + this.padding; 
   }
-  
+
+  addCoin() {
+    this.coins.push(this.coinImage);
+  }
+
+  draw() {
+    const totalCoins = this.coins.length;
+    const startX = this.position.x;
+    let currentX = startX;
+    let currentY = this.position.y;
+
+    this.coins.forEach((coin, index) => {
+      const x = currentX;
+      const y = currentY;
+
+      game.ctx.drawImage(coin, x, y, this.coinImageWidth, this.coinImageHeight);
+
+      if ((index + 1) % this.coinsPerRow === 0) {
+        currentX = startX;
+        currentY += this.rowHeight;
+      } else {
+        currentX += this.coinImageWidth + this.padding;
+      }
+    });
+  }
+}
