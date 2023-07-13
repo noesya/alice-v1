20ae0bb335cb1aff6e65e557cf036843b3885017
cle0L <95277869+cle0L@users.noreply.github.com>
2023-07-13 15:19:45 +0200

    all-cities

--- assets/js/data/cities.js
+++ assets/js/data/cities.js
@@ -58,36 +58,27 @@ export const CITIES = [
         srcHeight: 423,
     },
     {
-        id: 'kryptogarten',
-        name: 'Bankochain',
-        baseline: 'Les pauvres ont le droit de devenir riches',
+        id: 'bankochain',
         src: '/assets/images/cities/bankochain.png',
         x: 1000,
         y: 600,
         srcWidth: 306,
         srcHeight: 383,
-        html: "<p>Bienvenue chez Bankochain, la seule banque qui vous rend riche ! Car les crypto-monnaies, ça monte, ça monte, et tout le monde en profite. Les petites notes à la fin du smart-contract ? Non, ce n’est rien du tout, ayez confiance. On m’informe à l’instant que le cours du Bitcoin vient encore de perdre 50%, profitez-en c’est une opportunité extraordinaire !</p><blockquote>Les pauvres ont trouvé un moyen de faire du fric sans nous, il faut vite taxer et arrêter ça</blockquote><p>Ah pendant que j’y pense, ne perdez pas votre mot de passe. C’est un détail absurde, vous allez rire, mais nous n’avons pas prévu de fonctionnalité pour le récupérer. C’est trop compliqué, on n’y arrive pas. Et puis après tout ce n’est pas grave ! Vous avez perdu toutes vos économies ? Ce sera regagné en un rien de temps, si vous prenez quelques risques. Et si vous n’aimez pas les risques, il y a des stablecoins, des crypto-monnaies absolument stables. Est-ce que c’est sûr ? Évidemment, puisqu’on vous le dit ! C’est garanti par des algorithmes ! Des machines, quoi ! C’est infaillible, ce ne sont pas des stupides humains ! Une information vient de m’arriver, le stablecoin Terra vient de s’écrouler, il est annulé. Que voulez-vous, l’innovation, c’est parfois de la destruction créatrice.</p>"
     },
     {
-        id: 'kryptogarten',
-        name: 'Mordor Club',
-        baseline: 'Vas-y régule moi ça ma gourmande',
+        id: 'mordor-club',
         src: '/assets/images/cities/mordor-club.png',
         x: 2200,
         y: -100,
         srcWidth: 375,
         srcHeight: 401,
-        html: "<p>Le Mordor Club est un concept révolutionnaire : un club de musculation réservé aux hommes, aux vrais, les cryptobros. C’est aussi un haut lieu de socialisation, permettant de se gausser ensemble de tout ce qui n’est pas crypto, et de jouir entre frères de la technophilie crypto phallique. On y fanfaronne en montrant ses tweets d’insultes, de menaces et d’agressions, souvent sexuelles. On se souvient avec émotion des raids orchestrés contre les hérétiques, les incroyants, les sorcières anti&#8209;cryptos.</p><blockquote>Sinon les rêgles elle peut les rouler et se les insérer dans le fion?</blockquote><p>On y voit exploser les violences des âmes blessées par les critiques contre leur paradis artificiel. On y voit la colère et la détestation se déverser en torrents masculinistes, en fantaisies de toute puissance dignes d’enfants de 5 ans. On notera la contribution du Mordor Club à l’assouplissement des règles orthographiques et syntaxiques, de nombreux membres semblant atteints de troubles dysorthographiques et dyslexiques.</p>"
     },
     {
-        id: 'kryptogarten',
-        name: 'Cimetière du NFT',
-        baseline: 'Ci-gît la défunte DAO',
+        id: 'cimetiere-du-nft',
         src: '/assets/images/cities/cimetiere-du-nft.png',
         x: 1600,
         y: 160,
         srcWidth: 387,
         srcHeight: 426,
-        html: "<p>FTX, Terra et Luna, Celsius, 3 Arrows Capital, c’est ici que l’on commémore les morts au front. Parce qu’il ne faudrait pas croire que la cryptolife est un long fleuve tranquille ! Il y a des hauts, très hauts, aussi hauts qu’une immense pyramide de Ponzi, et des bas, très bas, comme une faillite.</p><blockquote>Qui aurait pu prédire la crise cryptographique ?</blockquote><p>Mais n'ayez aucune crainte, la technologie est si formidable qu'on pourra certainement ressusciter les morts. Bientôt... Il faut penser à long-terme.</p>"
     }
 ];
